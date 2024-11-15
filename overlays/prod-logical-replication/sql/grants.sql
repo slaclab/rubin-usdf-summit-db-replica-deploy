a6553fe088cccbb91e9e4f3f80d8d6b255b0d6f0
dspeck1 <dspeck@burwood.com>
2024-11-15 10:43:12 -0600

    Add USDF postgres account grants.

--- overlays/prod-logical-replication/sql/grants.sql
+++ overlays/prod-logical-replication/sql/grants.sql
@@ -31,3 +31,25 @@ ALTER DEFAULT PRIVILEGES FOR USER rubinadmin in SCHEMA public GRANT SELECT, INSE
 grant all privileges on database exposurelog to rubinadmin;
 grant all privileges on database narrativelog to rubinadmin;
 grant all privileges on database nightreport to rubinadmin;
+grant all privileges on database postgres to rubinadmin;
+
+
+-- summit_ro_usdf_rw
+-- ro
+GRANT USAGE ON SCHEMA public TO summit_ro_usdf_rw;
+GRANT SELECT ON ALL TABLES in SCHEMA PUBLIC to summit_ro_usdf_rw;
+GRANT SELECT ON public.alembic_version TO summit_ro_usdf_rw;
+GRANT SELECT ON public. message TO summit_ro_usdf_rw;
+GRANT USAGE ON SCHEMA cdb TO summit_ro_usdf_rw;
+GRANT USAGE ON SCHEMA cdb_latiss TO summit_ro_usdf_rw;
+GRANT USAGE ON SCHEMA cdb_lsstcam TO summit_ro_usdf_rw;
+GRANT USAGE ON SCHEMA cdb_lsstcamsim TO summit_ro_usdf_rw;
+GRANT USAGE ON SCHEMA cdb_lsstcomcam TO summit_ro_usdf_rw;
+GRANT USAGE ON SCHEMA cdb_lsstcomcamsim TO summit_ro_usdf_rw;
+GRANT SELECT ON ALL TABLES IN SCHEMA cdb_latiss TO summit_ro_usdf_rw;
+GRANT SELECT ON ALL TABLES IN SCHEMA cdb_lsstcam TO summit_ro_usdf_rw;
+GRANT SELECT ON ALL TABLES IN SCHEMA cdb_lsstcamsim TO summit_ro_usdf_rw;
+GRANT SELECT ON ALL TABLES IN SCHEMA cdb_lsstcomcam TO summit_ro_usdf_rw;
+GRANT SELECT ON ALL TABLES IN SCHEMA cdb_lsstcomcamsim TO summit_ro_usdf_rw;
+-- rw
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA cdb TO summit_ro_usdf_rw;

93755b9c20dd954403fe9f2dbc2fa5e0095c4b8e
dspeck1 <dspeck@burwood.com>
2025-03-12 13:58:45 -0500

    Add alembic bootstraps.

---
+++ overlays/prod-logical-replication/sql/cdb/alembic-clean.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS cdb.cdb_latiss_version;
+DROP TABLE IF EXISTS cdb.cdb_lsstcomcam_version;
+DROP TABLE IF EXISTS cdb.cdb_lsstcomcamsim_version;
+DROP TABLE IF EXISTS cdb.cdb_startrackerfast_version;
+DROP TABLE IF EXISTS cdb.cdb_startrackernarrow_version;
+DROP TABLE IF EXISTS cdb.cdb_startrackerwide_version;
---
+++ overlays/prod-logical-replication/sql/cdb/alembic.sql
@@ -0,0 +1,53 @@
+--
+-- Name: cdb_latiss_version; Type: TABLE; Schema: cdb; Owner: oods
+--
+
+CREATE TABLE cdb.cdb_latiss_version (
+    version_num character varying(32) NOT NULL
+);
+ALTER TABLE cdb.cdb_latiss_version OWNER TO oods;
+
+--
+-- Name: cdb_lsstcomcam_version; Type: TABLE; Schema: cdb; Owner: oods
+--
+
+CREATE TABLE cdb.cdb_lsstcomcam_version (
+    version_num character varying(32) NOT NULL
+);
+ALTER TABLE cdb.cdb_lsstcomcam_version OWNER TO oods;
+
+--
+-- Name: cdb_lsstcomcamsim_version; Type: TABLE; Schema: cdb; Owner: oods
+--
+
+CREATE TABLE cdb.cdb_lsstcomcamsim_version (
+    version_num character varying(32) NOT NULL
+);
+ALTER TABLE cdb.cdb_lsstcomcamsim_version OWNER TO oods;
+
+--
+-- Name: cdb_startrackerfast_version; Type: TABLE; Schema: cdb; Owner: oods
+--
+
+CREATE TABLE cdb.cdb_startrackerfast_version (
+    version_num character varying(32) NOT NULL
+);
+ALTER TABLE cdb.cdb_startrackerfast_version OWNER TO oods;
+
+--
+-- Name: cdb_startrackernarrow_version; Type: TABLE; Schema: cdb; Owner: oods
+--
+
+CREATE TABLE cdb.cdb_startrackernarrow_version (
+    version_num character varying(32) NOT NULL
+);
+ALTER TABLE cdb.cdb_startrackernarrow_version OWNER TO oods;
+
+--
+-- Name: cdb_startrackerwide_version; Type: TABLE; Schema: cdb; Owner: oods
+--
+
+CREATE TABLE cdb.cdb_startrackerwide_version (
+    version_num character varying(32) NOT NULL
+);
+ALTER TABLE cdb.cdb_startrackerwide_version OWNER TO oods;
